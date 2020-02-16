c96bb4d72734d6746c6d697ad8fd3ad94c723a60
Carl Meijer <carl.meijer@oracle.com>
2020-02-16 08:19:10 +0200

    Add JavaDoc comments.

--- src/main/java/com/hammingweight/spring/data/mongodb/encrypt/configuration/AutoConfiguration.java
+++ src/main/java/com/hammingweight/spring/data/mongodb/encrypt/configuration/AutoConfiguration.java
@@ -11,17 +11,24 @@ import java.util.Base64;
 
 @Configuration
 @ConditionalOnProperty("hammingweight.spring.data.mongodb.encrypt.key")
+/**
+ * Autoconfigures Spring Data Encryption for MongoDB if the Configuration property
+ * hammingweight.spring.data.mongodb.encrypt.key contains a 128 bit AES key encoded
+ * in base64.
+ */
 public class AutoConfiguration {
 
+    // A 128-bit AES key encoded in base64.
     @Value("${hammingweight.spring.data.mongodb.encrypt.key}")
-    private String secretKey;
+    private String secretKeyInBase64;
 
+    // Flag indicating whether to ignore decryption failures.
     @Value("${hammingweight.spring.data.mongodb.encrypt.silent-decryption-failures:false}")
     private boolean silentDecryptionFailures;
 
     @Bean
-    public CryptVault cryptVault() throws Exception {
-        byte[] secretKeyBytes = Base64.getDecoder().decode(secretKey);
+    CryptVault cryptVault() throws Exception {
+        byte[] secretKeyBytes = Base64.getDecoder().decode(secretKeyInBase64);
         return new CryptVault()
                 .with256BitAesCbcPkcs5PaddingAnd128BitSaltKey(0, secretKeyBytes)
                 .withDefaultKeyVersion(0);
